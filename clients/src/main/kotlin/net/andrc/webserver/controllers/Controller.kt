d3ace5155aa5b84249464e4656c71800fe35dc95
Unknown <andr.crus@gmail.com>
2019-05-04 23:54:16 +0300

    little refactoring

--- clients/src/main/kotlin/net/andrc/webserver/controllers/Controller.kt
+++ clients/src/main/kotlin/net/andrc/webserver/controllers/Controller.kt
@@ -14,6 +14,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.lang.StringBuilder
 import java.security.KeyPairGenerator
 import java.security.SecureRandom
 
@@ -71,7 +72,18 @@ class Controller(rpc: NodeRPCConnection, val cordaDialogService: CordaDialogServ
     }
 
     @GetMapping(value = ["/containers/registered"], produces = ["application/json"])
-    fun vaccinationRecords(): String = proxy.vaultQuery(PutContainerState::class.java).states.toJson()
+    fun vaccinationRecords(): String {
+        val builder = StringBuilder("[")
+        val iterator = proxy.vaultQuery(PutContainerState::class.java).states.iterator()
+        while (iterator.hasNext()) {
+            builder.append(iterator.next().state.data.toString())
+            if (iterator.hasNext()) {
+                builder.append(",\n")
+            }
+        }
+        builder.append("]")
+        return builder.toString()
+    }
 
     @GetMapping(value = ["/containers/all"], produces = ["application/json"])
     fun allContainers(): String = cordaDialogService.rootBoxService.getAll().toString()
--- clients/src/main/kotlin/net/andrc/webserver/cordaCommon/Utils.kt
+++ clients/src/main/kotlin/net/andrc/webserver/cordaCommon/Utils.kt
@@ -1,7 +1,5 @@
 package net.andrc.webserver.cordaCommon
 
-import net.andrc.states.PutContainerState
-import net.corda.core.contracts.StateAndRef
 import net.corda.core.transactions.SignedTransaction
 
 
@@ -11,17 +9,4 @@ fun SignedTransaction.toJson(): String {
         |"id" : "${this.id}",
         |"sigs: "${this.sigs}"
         |}""".trimMargin()
-}
-
-fun List<StateAndRef<PutContainerState>>.toJson(): String {
-    val builder = StringBuilder("[")
-    val itt = iterator()
-    while (itt.hasNext()) {
-        builder.append(itt.next().state.data.toString())
-        if (itt.hasNext()) {
-            builder.append(",\n")
-        }
-    }
-    builder.append("]")
-    return builder.toString()
 }
